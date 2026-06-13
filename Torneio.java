0e144d4e8c068d5cd3f680c00ab0045c417afa86
Deivid Beise <deividbeise@gmail.com>
2026-06-12 21:39:49 -0300

    adicionei 8 para lutadores

--- Torneio.java
+++ Torneio.java
@@ -12,7 +12,7 @@ public class Torneio {
             String classeMagia      = "Magia";
 
             // Vetores para armazenar os dados dos 8 lutadores
-            int MAX = 9;
+            int MAX = 8;
             String[] nomes      = new String[MAX];
             String[] classes    = new String[MAX];
             int[]    ataques    = new int[MAX];
